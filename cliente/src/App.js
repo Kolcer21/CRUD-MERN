7c2904ec42f6e84d54964cde12193923bc054a98
Kolcer21 <kevin.lopez5225@alumnos.udg.mx>
2023-05-30 21:11:26 +0200

    Enrutado con react-react-dom

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import ListaUsuarios from './ListaUsuarios';
 import AgregarUsuario from './AgregarUsuario';
 import EditarUsuario from './EditarUsuario';
+import {BrowserRouter, Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>CRUD MERN Stack</h1>
-      <ListaUsuarios/>
-      <AgregarUsuario/>
-      <EditarUsuario/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListaUsuarios/>} exact/>
+          <Route path="/agregarusuario" element={<AgregarUsuario/>}exact/> 
+          <Route path="/editarusuario" element={<EditarUsuario/>}exact/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
