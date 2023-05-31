acc1220b8ee9c55297b3dde360e1982b52168b33
Kolcer21 <kevin.lopez5225@alumnos.udg.mx>
2023-05-31 21:04:53 +0200

    Creando vistas para formulario de usuarios

--- cliente/src/AgregarUsuario.js
+++ cliente/src/AgregarUsuario.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 function AgregarUsuario()
 {
+
+    function agregarUsuario()
+    {
+        
+    }
+
     return (
-        <div>
-            <h2>Crear un nuevo usuario</h2>
+        <div className='container m-auto'>
+            <div className='row'>   
+                <div className='col-sm-6'>
+                     <h2 className='mt-4'>Crear un nuevo usuario</h2>
+                </div>
+            </div>
+           
+            <div className='row'>   
+                <div className='col-sm-6 offset-43'>
+                     <div className='mb-3'>
+                        <label hmtlFor='nombre' className='form-label'>Nombre</label>
+                        <input type='text' className='form-control' id='nombre' placeholder='Ingrese su nombre'/>
+                     </div>
+                     <div className='mb-3'>
+                        <label hmtlFor='email' className='form-label'>Email</label>
+                        <input type='email' className='form-control' id='nombre' placeholder='Ingrese su email'/>
+                     </div>
+                     <div className='mb-3'>
+                        <label hmtlFor='telefono' className='form-label'>Telefono</label>
+                        <input type='text' className='form-control' id='nombre' placeholder='Ingrese su telefono'/>
+                     </div>
+
+                        <button type='submit' onClick={agregarUsuario} className='btn btn-succes'>Guardar</button>
+
+                </div>
+            </div>
+
         </div>
     );
 }
--- cliente/src/App.js
+++ cliente/src/App.js
@@ -7,7 +7,25 @@ import {BrowserRouter, Router, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1>CRUD MERN Stack</h1>
+
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container">
+    <a className="navbar-brand" href="#">CRUD MERN Stack</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="/">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="agregarusuario">Agregar Usuario</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
       <BrowserRouter>
         <Routes>
